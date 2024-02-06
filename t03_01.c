a74ff862933e2b36637736f6e165907d34111ec1
ChintyaRajagukguk <143678980+ChintyaRajagukguk@users.noreply.github.com>
2024-02-06 21:38:04 +0700

    updated

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,25 @@
-// NIM - NAMA
+// 12S3045 - Chintya Reginauli Rajagukguk
 
-#include <stdio.h>
+ #include <stdio.h>
 
 int main(int _argc, char **_argv)
 {
+  int n, angka, min = 100, max = -100;
 
-  // your code here
+  scanf("%d", &n);
+
+  for(int i = 0; i < n; i++) {
+    scanf("%d", &angka);
+    if(angka < min) {
+      min = angka;
+    }
+    if(angka > max) {
+      max = angka;
+    }
+  }
+
+  printf("%d\n", min);
+  printf("%d\n", max);
 
   return 0;
 }
--- t03_02.c
+++ t03_02.c
@@ -5,7 +5,25 @@
 int main(int _argc, char **_argv)
 {
 
-  // your code here
+    int n, angka, min = 100, max = -100, average;
+
+  scanf("%d", &n);
+
+  for(int i = 0; i < n; i++) {
+    scanf("%d", &angka);
+    if(angka < min) {
+      min = angka;
+    }
+    if(angka > max) {
+      max = angka;
+    }
+  }
+
+  average = sum += angka / n;
+
+  printf("%d\n", min);
+  printf("%d\n", max);
+  printf("%.2f\n", average);
 
   return 0;
 }
